042cd234da440aade4b91d6dc81d1acd2c830314
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-11-06 14:08:11 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@ const dummyTransactions = [
   { id:1, name: 'Bolo De Brigadeiro', amount: -20},
   { id:2, name: 'Salário', amount: 3000},
   { id:3, name: 'Torta de Frango', amount: -10},
-  { id:4, name: 'Livro', amount: 150}
+  { id:4, name: 'Batom Luna', amount: 150}
 ]
+
+const addTransactionIntoDOM = transaction => {
+  
+}
