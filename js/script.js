458de7c06ef38633177f72e084fce450c4cf5f7f
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-11-06 15:04:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 const dummyTransactions = [
-  { id:1, name: 'Bolo De Brigadeiro', amount: -20},
+  { id:1, name: 'Bolo De Brigadeiro', amount: -20},k
   { id:2, name: 'Salário', amount: 3000},
   { id:3, name: 'Torta de Frango', amount: -10},
   { id:4, name: 'Batom Luna', amount: 150}
 ]
 
 const addTransactionIntoDOM = transaction => {
-  <li class="minus">
-       Salário <span>-R$4000</span><button class="delete-btn"
-  </li>
+  
+ /* <li class="minus">
+    Salário <span>-R$4000</span><button class="delete-btn">x</button>
+  </li> */
 }
